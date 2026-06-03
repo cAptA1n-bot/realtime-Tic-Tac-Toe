beec3d7913043478fb21d03c6bf725987576c91a
Prince Yadav <prince1bot001@gmail.com>
2026-06-03 18:39:19 +0530

    feat: frontend created

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const Board = () => {
-  return (
-    <div>
-      Board
+     const board = Array(9).fill("");
+    return (
+    <div className="min-h-screen flex items-center justify-center bg-base-200">
+      <div className="grid grid-cols-3 gap-2">
+        {board.map((cell, index) => (
+          <button
+            key={index}
+            className="btn btn-primary btn-square w-24 h-24 text-4xl font-bold"
+          >
+            {cell}
+          </button>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Board
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 const Home = () => {
+    const[inputField, setInputField] = useState(false);
+    const joinRoom = () => {
+        setInputField(false);
+    }
   return (
     <div>
-      
+        {inputField &&
+            <div className="fixed top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-150 h-75 z-50">
+                <div className="bg-black p-6 rounded-lg h-full flex flex-col justify-around items-center">
+                    <div className="text-white text-3xl font-bold text-center">Join Room</div>
+                    <input type="text" placeholder="Enter Room Id" className="input input-bordered w-full max-w-xs text-2xl" />
+                    <button className="btn btn-secondary text-2xl w-40" onClick={joinRoom}>Join</button>
+                </div>
+            </div>
+        }
+      <div className="flex justify-center items-center h-screen gap-4">
+        <button className="btn btn-primary w-50 h-30 text-2xl" onClick={() => setInputField(true)}>Join Room</button>
+        <button className="btn btn-primary w-50 h-30 text-2xl">Create Room</button>
+      </div>
     </div>
   )
 }
